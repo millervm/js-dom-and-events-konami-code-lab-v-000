881d8ba2a079034d43bc846df89b7bab298e731f
Veronica Miller <vmcmiller@gmail.com>
2019-04-28 19:48:43 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-document.body.addEventListener("keydown", init());
-document.body.addEventListener("click", greeting());
+document.addEventListener("keydown", init());
+document.addEventListener("click", greeting());
 
 function greeting() {
   alert("Hello!");
