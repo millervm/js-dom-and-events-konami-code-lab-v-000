dd81fe218d4b1f389cad9520cf556f2e9f10766c
Veronica Miller <vmcmiller@gmail.com>
2019-04-28 19:50:33 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,7 +11,7 @@ let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const key = parseInt(e.detail || e.which);
+  const key = parseInt(this.detail || this.which);
 
   let index = 0;
 
