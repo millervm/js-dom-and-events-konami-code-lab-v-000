9ea7550a910f250dac53089a97a5918d34272b92
Veronica Miller <vmcmiller@gmail.com>
2019-04-28 19:54:33 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ function init() {
 
   if (key === code[index]) {
     index++;
-    if (index === code.length - 1) {
+    if (index === code.length) {
       alert("Konami!");
       index = 0;
     }
