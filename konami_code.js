a0ba74b0746d204c5b858a9d7fdcd9d3081bf08c
Veronica Miller <vmcmiller@gmail.com>
2019-04-28 19:35:55 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+document.addEventListener("keydown", init())
+
 let index = 0;
 
-function init(e) {
+function init() {
   // Write your JavaScript code inside the init() function
-  const key = parseInt(e.detail || e.which);
+  const key = parseInt(this.detail || this.which);
 
   if (key === code[index]) {
     index++;
